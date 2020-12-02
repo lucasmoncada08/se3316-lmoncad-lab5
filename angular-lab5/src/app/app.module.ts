f9959e028ce4d5f809caa0bc117ca74efccf50c8
Lucas Moncada <lucas.moncada08@gmail.com>
2020-12-02 12:01:01 -0500

    Added Search for Courses Skeleton

--- angular-lab5/src/app/app.module.ts
+++ angular-lab5/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { CourseSearchComponent } from './course-search/course-search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CourseSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
