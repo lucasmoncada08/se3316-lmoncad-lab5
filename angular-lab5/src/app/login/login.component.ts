e925c6e2c0e5d1b10c65116a86cbfbbcd587bb7c
Lucas Moncada <lucas.moncada08@gmail.com>
2020-12-01 23:23:29 -0500

    Added Login Aesthetics

--- angular-lab5/src/app/login/login.component.ts
+++ angular-lab5/src/app/login/login.component.ts
@@ -4,6 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
+
+
 export class LoginComponent {
+
+  onLogin() {
+
+  }
+
+  onNewUser() {
+
+  }
 
 }
