31d2871adc3fca351e909565a1147098b1c73143
Lucas Moncada <lucas.moncada08@gmail.com>
2020-12-22 14:43:31 -0500

    Added Local Storage for Refreshing and Staying Logged In

--- angular-lab5/src/app/auth/auth.service.ts
+++ angular-lab5/src/app/auth/auth.service.ts
@@ -42,11 +42,12 @@ export class AuthService {
             alert('Account is deactivated, please contact timetableadmin@uwo.ca');
           else {
             const expiresInDuration = res.expiresIn
-            this.tokenTimer = setTimeout(() => {
-              this.logout();
-            }, expiresInDuration*1000);
+            this.setAuthTimer(expiresInDuration);
             this.isAuthenticated = true;
             this.authStatusListener.next(true);
+            const now = new Date();
+            const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+            this.saveAuthData(res.token, expirationDate);
             this.router.navigate(['/courselists']);
           }
         }
@@ -55,12 +56,57 @@ export class AuthService {
       })
   }
 
+  autoAuthUser() {
+    const authInfo = this.getAuthData();
+    if (!authInfo) {
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInfo.expirationDate.getTime() - now.getTime();
+    if (expiresIn > 0) {
+      this.token = authInfo.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn / 1000)
+      this.authStatusListener.next(true);
+    }
+  }
+
   logout() {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
     clearTimeout(this.tokenTimer);
+    this.clearAuthData();
     this.router.navigate(['/login']);
+  }
+
+  private setAuthTimer(duration: number) {
+    console.log('Setting timer: ' + duration);
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration*1000);
+  }
+
+  private saveAuthData(token: string, expirationDate: Date) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expiration');
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token: token,
+      expirationDate: new Date(expirationDate)
+    }
   }
 
 }
--- angular-lab5/src/app/header/header.component.ts
+++ angular-lab5/src/app/header/header.component.ts
@@ -15,6 +15,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   constructor(private authService: AuthService) {};
 
   ngOnInit() {
+    this.userIsAuthenticated = this.authService.getIsAuth();
     this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
       this.userIsAuthenticated = isAuthenticated;
     });
@@ -22,6 +23,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.authListenerSubs.unsubscribe();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
 }
